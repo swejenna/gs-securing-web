d5ee481e97a44e95d71f4556351ebd1c297f2249
Jay <108143448+swejenna@users.noreply.github.com>
2025-08-13 18:42:06 -0800

    eod commit

--- complete/src/main/java/com/example/securingweb/MvcConfig.java
+++ complete/src/main/java/com/example/securingweb/MvcConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	@SuppressWarnings("null")
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
